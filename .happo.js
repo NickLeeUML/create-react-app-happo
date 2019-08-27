cbc65ef361841bbb8bc2f93896a1cbe2c30bf74f
Nick Lee <Nicholas_Lee@uml.edu>
2019-08-27 16:44:19 -0400

    added set up files

---
+++ .happo.js
@@ -0,0 +1,13 @@
+const { RemoteBrowserTarget } = require('happo.io');
+
+module.exports = {
+    apiKey: '0d883eaa86',
+    apiSecret: '06013ad6d1e96f6309d280d48',
+    pages: [
+          { url: 'http://cda9c358.ngrok.io/Website.UI.Template.v6.happo-url/patterns/components-crc-headcount-loader.inside.html', title: 'Current Useage' },
+          { url: 'http://cda9c358.ngrok.io/Website.UI.Template.v6.happo-url/patterns/components-faq-default.inside.html', title: 'FAQ', variant: "inside"},
+          { url: 'http://cda9c358.ngrok.io/Website.UI.Template.v6.happo-url/patterns/apps-health-provider-search-default.inside.html', title: 'Health Providers', variant: "inside"},
+          { url: 'http://cda9c358.ngrok.io/Website.UI.Template.v6.happo-url/patterns/layout-header-default.default.html', title: 'header', variant: "inside" },
+          { url: 'https://www.uml.edu', title: 'home page'}
+        ],
+  };
