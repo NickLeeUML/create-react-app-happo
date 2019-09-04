c7be05f6c99ec7a789ad766f4768b20f028d3b94
Nick Lee <Nicholas_Lee@uml.edu>
2019-09-04 13:08:52 -0400

    update happo.js, urls and happo package

--- .happo.js
+++ .happo.js
@@ -4,10 +4,7 @@ module.exports = {
     apiKey: '0d883eaa86',
     apiSecret: '06013ad6d1e96f6309d280d48',
     pages: [
-          { url: 'http://8f65eef7.ngrok.io/Website.UI.Template.v6.happo-url/patterns/components-crc-headcount-loader.inside.html', title: 'Current Useage' },
-          { url: 'http://8f65eef7.ngrok.io/Website.UI.Template.v6.happo-url/patterns/components-faq-default.inside.html', title: 'FAQ', variant: "inside"},
-          { url: 'http://8f65eef7.ngrok.io/Website.UI.Template.v6.happo-url/patterns/apps-health-provider-search-default.inside.html', title: 'Health Providers', variant: "inside"},
-          { url: 'http://8f65eef7.ngrok.io/Website.UI.Template.v6.happo-url/patterns/layout-header-default.default.html', title: 'header', variant: "inside" },
+          { url: 'https://www.uml.edu/student-life/', title: 'student life'},
           { url: 'https://www.uml.edu/', title: 'home page'},
         ],
   };
